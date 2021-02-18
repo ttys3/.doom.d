8c25849c55a0cffa54b04121cc94401d8e8ec020
ttys3 <ttys3@outlook.com>
2021-02-18 17:43:18 +0800

    fix: fixup rainbow delimiters for other langs not working problem
    ref to https://huadeyu.tech/tools/emacs-setup-notes.html

--- config.el
+++ config.el
@@ -147,11 +147,14 @@
 ;; recover max face count limit to 3 by doom emacs in core/core-ui.el
 ;; and also add rainbow delimiters for all langs (doom only enabled it in few langs)
 ;; https://github.com/Fanael/rainbow-delimiters
-(after! rainbow-delimiters
-(progn
-        (setq rainbow-delimiters-max-face-count 9)
-        (add-hook 'prog-mode-hook #'rainbow-delimiters-mode))
-)
+
+(use-package! rainbow-delimiters
+  :config
+  (progn
+    (defun @-enable-rainbow-delimiters ()
+      (rainbow-delimiters-mode t))
+    (setq rainbow-delimiters-max-face-count 9)
+    (add-hook 'prog-mode-hook '@-enable-rainbow-delimiters)))
 
 (use-package! easy-hugo
    :config
