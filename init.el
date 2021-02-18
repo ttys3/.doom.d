d986625e7f404ebc6d5ea2c0f27694de5ac2fa50
ttys3 <ttys3@outlook.com>
2021-02-18 19:31:24 +0800

    feat(init): enable docker and web (for html and css) module

--- init.el
+++ init.el
@@ -109,7 +109,7 @@
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
@@ -187,7 +187,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       web               ; the tubes
        yaml              ; JSON, but readable
 
        :email
